4ae8cc445d84e2b356534c8c0d5f95bd29037c5d
mengMia <mengya@ahu.edu.cn>
2020-09-20 20:37:25 +0800

    爬楼梯

--- Week_01/LeetCode70.java
+++ Week_01/LeetCode70.java
@@ -1,5 +1,26 @@
 package my;
 
 public class LeetCode70 {
-
+    public int climbStairs(int n) {
+        if(n < 3) return n;
+        int fn2 = 1;
+        int fn1 = 2;
+        int fn = 0;
+        for(int j = 3; j<= n; j++){
+            fn = fn1 + fn2;
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        return fn;
+    }
+    /*public int climbStairs(int n) {
+        if (n == 1 || n == 2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int j = 3; j <= n; j++) {
+            dp[j] = dp[j - 1] + dp[j - 2];
+        }
+        return dp[n];
+    }*/
 }
