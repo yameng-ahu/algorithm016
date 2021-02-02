9222621d03da2f622af5fea7a3ea806140a60200
mengMia <mengya@ahu.edu.cn>
2021-02-02 20:52:32 +0800

    63.不同路径II-降低空间复杂度1

--- leetcode-sub/src/leetcode/editor/cn/UniquePathsIi.java
+++ leetcode-sub/src/leetcode/editor/cn/UniquePathsIi.java
@@ -41,24 +41,26 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = m-1; i >= 0 && obstacleGrid[i][n-1] == 0; i--) {
-            dp[i][n-1] = 1;
-        }
-        //或者写成:
-        // for (int i = m -1; i >= 0; i--) {
-        //     if (obstacleGrid[i][n - 1] == 1) break;
-        //     else dp[i][n-1] = 1;
-        // }
-        for (int j = n-1; j >= 0 && obstacleGrid[m-1][j] == 0; j--) {
-            dp[m-1][j] = 1;
-        }
-        for (int i = m - 2; i >= 0; i--){
-            for (int j = n - 2; j >= 0; j--){
-               if (obstacleGrid[i][j] == 0) dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        int[] pre = new int[n];
+        int[] cur = new int[n];
+        for (int j = n - 1; j >= 0; j--){
+            if (obstacleGrid[m - 1][j] == 1) break;
+            else {
+                cur[j] = 1;
+                pre[j] = 1;
             }
         }
-        return dp[0][0];
+        for (int i = m - 2; i >= 0; i--){
+            for (int j = n - 1; j >= 0; j--){
+                if (obstacleGrid[i][j] == 1) cur[j] = 0;
+                else {
+                    if (j == n - 1) cur[j] = pre[j];
+                    else cur[j] = pre[j] + cur[j+1];
+                }
+            }
+            pre = cur;
+        }
+        return cur[0];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
