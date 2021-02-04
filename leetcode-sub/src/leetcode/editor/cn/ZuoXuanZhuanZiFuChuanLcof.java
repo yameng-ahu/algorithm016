625d08bf2217c1e2f7ed2109d7c511c7e2de7821
mengMia <mengya@ahu.edu.cn>
2021-02-05 06:06:05 +0800

    剑指Offer58-II左旋转字符串-使用StringBuilder直接先添加后面的再添加前面的

--- leetcode-sub/src/leetcode/editor/cn/ZuoXuanZhuanZiFuChuanLcof.java
+++ leetcode-sub/src/leetcode/editor/cn/ZuoXuanZhuanZiFuChuanLcof.java
@@ -35,21 +35,28 @@ public class ZuoXuanZhuanZiFuChuanLcof{
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public String reverseLeftWords(String s, int n) {
-        StringBuilder s_before = new StringBuilder();
-        StringBuilder s_after = new StringBuilder();
-        int count = 0;
-        while (count < n){
-            char c_b = s.charAt(count);
-            s_before.append(c_b);
-            count++;
-        }
+    /*public String reverseLeftWords(String s, int n) {
+        //使用java中的substring
+        return s.substring(n, s.length()) + s.substring(0, n);
+    }*/
+    /*public String reverseLeftWords(String s, int n) {
+        //使用StringBuilder的优化
+        StringBuilder res = new StringBuilder();
         for (int i = n; i < s.length(); i++){
-            char c_a = s.charAt(i);
-            s_after.append(c_a);
+            res.append(s.charAt(i));
         }
-        s_after.append(s_before);
-        return s_after.toString();
+        for (int i = 0; i < n; i++){
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }*/
+    public String reverseLeftWords(String s, int n) {
+        //使用StringBuilder的优化
+        StringBuilder res = new StringBuilder();
+        for (int i = n; i < s.length() + n; i++){
+            res.append(s.charAt(i % s.length()));
+        }
+        return res.toString();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
