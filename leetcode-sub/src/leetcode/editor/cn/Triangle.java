5e2aee663917b69bd089eeb7ee34d1215c47b935
mengMia <mengya@ahu.edu.cn>
2021-02-04 19:11:42 +0800

    120.三角形的最小路径和-动态规划-降低空间复杂度为o（n）

--- leetcode-sub/src/leetcode/editor/cn/Triangle.java
+++ leetcode-sub/src/leetcode/editor/cn/Triangle.java
@@ -42,13 +42,14 @@ class Solution {
     //c.dp方程，dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n+1][n+1];
+        int[] dp = new int[n + 1];
         for (int i = n - 1; i >= 0; i--){
             for (int j = 0; j <= i; j++){
-                dp[i][j] = Math.min(dp[i+1][j], dp[i+1][j+1]) + triangle.get(i).get(j);
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
             }
         }
-        return dp[0][0];
+        return dp[0];
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
